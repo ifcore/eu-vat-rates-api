db7712be8e182f3f6c282e347e86c36d79100880
Felix Ebert <felix.ebert@if-core.de>
2021-10-10 22:42:30 +0200

    standard vat rate extraction test: fix incorrect vat rate for spain

--- src/test/java/de/ifcore/commons/euvatratesapi/EuVatRatesApiApplicationTests.java
+++ src/test/java/de/ifcore/commons/euvatratesapi/EuVatRatesApiApplicationTests.java
@@ -12,6 +12,7 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class EuVatRatesApiApplicationTests {
@@ -19,15 +20,16 @@ class EuVatRatesApiApplicationTests {
     private EuVatRatesClient euVatRatesClient;
 
     @Test
-    void testRequest() {
+    void testGetStandardRates() {
         LocalDate date = LocalDate.of(2021, 1, 1);
         RetrieveVatRatesRespType response = euVatRatesClient.getVatRates(EuVatRatesClient.EU_COUNTRIES, date);
         Set<StandardVatRate> vatRates = response.vatRateResults.stream()
                 .filter(vatRateResults -> RateTypeEnum.STANDARD.equals(vatRateResults.type)
                         && RateValueTypeEnum.DEFAULT.equals(vatRateResults.rate.type))
                 .map(vatRateResults -> new StandardVatRate(vatRateResults.memberState, vatRateResults.rate.getValue()))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toCollection(TreeSet::new));
 
+        // simplification: use the highest rate of a country in case there are multiple (like in Spain, 7% for Canary Islands)
         List<String> countries = new ArrayList<>();
         Set<StandardVatRate> uniqueVatRates = new TreeSet<>();
         for (StandardVatRate vatRate : vatRates) {
@@ -38,5 +40,8 @@ class EuVatRatesApiApplicationTests {
         }
 
         assertEquals(uniqueVatRates.size(), EuVatRatesClient.EU_COUNTRIES.size());
+        for (StandardVatRate vatRate : uniqueVatRates) {
+            assertTrue(vatRate.getRate() >= 17);
+        }
     }
 }
